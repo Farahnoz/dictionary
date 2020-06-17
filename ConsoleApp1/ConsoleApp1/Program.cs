7f6a12307b2acfc94bb69525efc52cb4729fd1e6
Farahnoz <farahnoz.m1999@gmail.com>
2020-06-17 18:34:09 +0500

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> people = new Dictionary<char, string>();
+            people.Add('a',"Fara");
+            people.Add('b',"Mary");
+            people.Add('c',"David");
+            foreach(KeyValuePair<char,string> KeyValues in people)
+            {
+                Console.WriteLine(KeyValues.Key+" - "+KeyValues.Value);
+            }
+            foreach(char c in people.Keys)
+            {
+                Console.WriteLine(c);
+            }
+            foreach(string p in people.Values)
+            {
+                Console.WriteLine(p);
+            }
+            Console.Read();
+        }
+    }
+}
